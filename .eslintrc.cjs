141a1531c57329642f7737e915dbcfea4cd8af00
Ngô Hữu Phước <nhph@netcompany.com>
2023-09-29 16:32:32 +0700

    Adjust eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,47 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
-  rules: {
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "max-len": [
-      "error",
-      {
-        code: 150,
-        ignoreRegExpLiterals: true,
-      },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        '@remix-run/eslint-config',
+        '@remix-run/eslint-config/node',
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'plugin:jsx-a11y/recommended',
+        'prettier'
     ],
-    "@typescript-eslint/semi": ["error", "always"],
-    "@typescript-eslint/triple-slash-reference": "off",
-    "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off",
-    "space-before-function-paren": [
-      "error",
-      {
-        anonymous: "always",
-        named: "never",
-        asyncArrow: "always",
-      },
-    ],
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname
+    },
+    plugins: ['react', 'jsx-a11y', 'prettier'],
+    rules: {
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        'max-len': [
+            'error',
+            {
+                code: 150,
+                ignoreRegExpLiterals: true
+            }
+        ],
+        '@typescript-eslint/semi': ['error', 'always'],
+        '@typescript-eslint/triple-slash-reference': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'always',
+                named: 'never',
+                asyncArrow: 'always'
+            }
+        ],
+        'prettier/prettier': 'error'
+    }
 };
