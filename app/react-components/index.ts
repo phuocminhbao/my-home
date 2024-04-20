6d729f0c102ce4b10fd6c00099d244fccb86f136
Huu Phuoc <tpgnor@gmail.com>
2024-04-20 18:28:53 +0700

    Removing unnessary components

--- app/react-components/index.ts
+++ app/react-components/index.ts
@@ -1,5 +1,3 @@
-import TableList from './TableList/TableList';
-import AccordionRow from './AccordionRow/AccordionRow';
 import Loading from './Loading/Loading';
 
-export { TableList, AccordionRow, Loading };
+export { Loading };
