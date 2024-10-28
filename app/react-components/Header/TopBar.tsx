6548dc9f474378ec4307a0e2fad20cf60f440e23
Hữu Phước <tpgnor@gmail.com>
2024-10-28 18:00:28 +0700

    Fix title button

--- app/react-components/Header/TopBar.tsx
+++ app/react-components/Header/TopBar.tsx
@@ -1,21 +1,40 @@
-import { AppBar, Box, Toolbar, Stack, Typography, Drawer, List, Divider } from '@mui/material';
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    Stack,
+    Typography,
+    Drawer,
+    List,
+    Divider,
+    Button
+} from '@mui/material';
 import LanguageChange from './LanguageChange';
 import ThemeChange from './ThemeChange';
 import useLayoutSize from '~/hook/useLayoutSize';
 import { Menu } from '@mui/icons-material';
 import { useState } from 'react';
+import { useNavigate } from '@remix-run/react';
 
 const TopBar = () => {
     const { isMobile } = useLayoutSize();
     const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+    const navigate = useNavigate();
     return (
         <>
             <AppBar position="fixed" color="primary">
                 <Toolbar>
                     <Stack direction="row" width="100%" justifyContent="space-between">
-                        <Box alignContent="center">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            disableElevation
+                            onClick={() => {
+                                navigate('/');
+                            }}
+                        >
                             <Typography variant="h6">VĂN TRỊ</Typography>
-                        </Box>
+                        </Button>
                         {isMobile ? (
                             <Box alignContent="center">
                                 <Menu
